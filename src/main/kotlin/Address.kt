8b33465a51a0a3c4af94b6f38de7bef30bace9a6
ROLXML <sahrul21082003@gmail.com>
2024-09-03 13:41:55 +0700

    Greeter

---
+++ src/main/kotlin/Address.kt
@@ -0,0 +1,14 @@
+class Address {
+    var name: String = "Holmes, Sherlock"
+    var street: String = "Baker"
+    var city: String = "London"
+    var state: String? = null
+    var zip: String = "123456"
+}
+fun copyAddress(address: Address): Address {
+    val result = Address() // there's no 'new' keyword in Kotlin
+    result.name = address.name // accessors are called
+    result.street = address.street
+// ...
+    return result
+}
